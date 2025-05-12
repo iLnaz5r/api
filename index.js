21f81f325cbd2e8f782bfb9e9acba8c339192262
Shaikhystan Elnaz <elnaz@MacBook-Pro-Shaikhystan.local>
2025-05-12 18:34:20 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// function getAdvice() {
+//     fetch('https://api.adviceslip.com/advice')
+//     .then(res => res.json())
+//     .then(data =>{
+//         document.getElementById('output').innerText = `${data.slip.advice}`;
+//     })
+//     .catch(error =>{
+//         document.getElementById('output').innerText = 'Smth went wrong!';
+//         console.error(error);
+//     });
+// }
